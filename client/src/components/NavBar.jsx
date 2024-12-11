7e7833293a9fec7908bd5c3a1c796b61cb52bb62
raghav <rghvrmn00@gmail.com>
2024-12-11 18:27:20 +0530

    idk navbar change

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect } from "react";
 import { navbarFetch } from "../services/navbarFetch";
 
 
-const categoriesArray = await navbarFetch();
+const categoriesArray = await navbarFetch() || [] 
 
  
 
