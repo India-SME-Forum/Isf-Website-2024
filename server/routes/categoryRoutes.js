d2c163f0e13e74d44a2159e0ea631ccbba20e534
raghav <rghvrmn00@gmail.com>
2024-12-16 18:31:17 +0530

    add delete category

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addCategories, addNewSubsection, addSingleCategory, getCategories } from "../controllers/addCategories.js";
+import { addCategories, addNewSubsection, addSingleCategory, deleteSingleCategory, getCategories, } from "../controllers/addCategories.js";
 const catRouter = express.Router();
 
 catRouter.post("/create", addCategories); // Bulk categoryAdd
 catRouter.get("/fetch", getCategories); // Fetch all categories
 catRouter.post("/createsingle", addSingleCategory); //Add single Category
 catRouter.post("/addnewsubsection", addNewSubsection); // Add subSection
-catRouter.delete('/deletesinglecategory',addSingleCategory) //delete single category
+catRouter.delete('/deletesinglecategory',deleteSingleCategory) //delete single category
 export default catRouter;
