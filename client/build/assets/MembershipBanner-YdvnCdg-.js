829175d65cc736012a1a1a1a4c0429860d1d991d
raghav <rghvrmn00@gmail.com>
2025-01-15 16:22:01 +0530

    add build

---
+++ client/build/assets/MembershipBanner-YdvnCdg-.js
@@ -0,0 +1,24 @@
+import{c as r,j as e}from"./index-CNp5HLUP.js";/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=r("Book",[["path",{d:"M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20",key:"k3hazp"}]]);/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=r("Calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]);/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=r("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=r("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]]),d=()=>{const a=[{title:"Become a Member",description:"Empower your business and contribute to India's thriving SME ecosystem today!",icon:e.jsx(n,{className:"w-8 h-8"}),bgColor:"bg-teal-600",textColor:"text-white",hoverBg:"hover:bg-teal-700"},{title:"Membership Benefits & Plans",description:"From emerging startups to established businesses, explore the wide range of advantages and plans tailored to your needs.",icon:e.jsx(l,{className:"w-8 h-8"}),bgColor:"text-gray-700",textColor:"bg-slate-200",hoverBg:"hover:bg-slate-300"},{title:"Member Directory",description:"Connect with India's vibrant SME community by accessing our comprehensive member directory.",icon:e.jsx(o,{className:"w-8 h-8"}),bgColor:"bg-amber-500",textColor:"text-white",hoverBg:"hover:bg-amber-600",url:"/member-directory"},{title:"Events",description:"Participate in over 100+ events annually designed to foster growth, learning, and collaboration.",icon:e.jsx(i,{className:"w-8 h-8"}),bgColor:"bg-orange-100",textColor:"text-gray-700",hoverBg:"hover:bg-orange-200"}];return e.jsx("div",{className:" max-w-full  my-5 2xl:px-44 mx-auto p-4",children:e.jsx("div",{className:"grid grid-cols-1 md:grid-cols-4 gap-0 rounded-lg overflow-hidden shadow-xl",children:a.map((t,s)=>e.jsx("div",{className:`${t.bgColor} ${t.textColor} ${t.hoverBg} 
+              p-6 transition-all duration-300 ease-in-out cursor-pointer
+              transform hover:-translate-y-1 hover:shadow-lg 
+              group relative`,children:e.jsxs("a",{href:t.url,children:[e.jsxs("div",{className:"flex flex-col space-y-4",children:[e.jsx("div",{className:"flex items-center justify-center md:justify-start",children:e.jsx("div",{className:"transition-transform duration-300 group-hover:scale-110",children:t.icon})}),e.jsx("h3",{className:"text-lg   text-center md:text-left",children:t.title}),e.jsx("p",{className:"text-sm text-center md:text-left opacity-90",children:t.description})]}),e.jsx("div",{className:"absolute bottom-0 left-0 w-full",children:e.jsx("div",{className:"h-0.5 bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"})})]})},s))})})};export{d as default};
