cee836909c24014e9ece269bf15ca22cb314a8ca
raghav <rghvrmn00@gmail.com>
2025-01-15 17:53:40 +0530

    hompage bug

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,16 +1,6 @@
 import React, { Suspense } from "react";
 
-import Navbar from "../components/navbar";
-// import Hero from "../components/Hero";
-// import MembershipBanner from "../components/MembershipBanner";
-// import MembershipStats from "../components/StatsMemeber";
-// import NewsXblogs from "../components/NewsXblogs";
-// import PartnerCarousel from "../components/Partners";
-// import SocialMediaBanner from "../components/Socials";
-// import Footer from "../components/Footer";
-// import MembershipBenefits from "../components/Memberbenefits";
-// import OurSupporters from "../components/OurSupporters";
-// import MemberCarousel from "../components/MemberCarousel";
+ 
 
 const MemberCarousel = React.lazy(() => import("../components/MemberCarousel"));
 const OurSupporters = React.lazy(() => import("../components/OurSupporters"));
