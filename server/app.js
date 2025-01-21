c92d2867baafa0848360f4b68bcbe23d57b013bd
raghav <rghvrmn00@gmail.com>
2025-01-21 16:42:20 +0530

    cors orign

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import { connectDB } from "./mongodb.js";
 import catRouter from "./routes/categoryRoutes.js";
@@ -10,19 +10,21 @@ const PORT = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL],
-  credentials: true,
+  origin: [process.env.FRONTEND_URL,'http://localhost:3000'],
+  credentials: false,
   methods: ["GET", "POST", "PUT", "DELETE"], //access-control-allow-credentials:true
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(express.json());
 
-app.use(cors(corsOptions));
+app.use(cors( corsOptions));
+// app.use('/' , express.static(path.join(__dirname, '../client/dist')))
 app.use("/api/v1/categories", catRouter);
 app.use(errorHandler);
 // mongo db connection
 connectDB();
-app.listen(PORT, async () => {
+app.listen(PORT, async (req,res) => {
+   
   console.log(`Server running at  ${PORT} in ${process.env.NODE_ENV} mode`);
 });
