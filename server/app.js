b88dea48b6080153d9581aad9a57b2a595f98b9c
raghav <rghvrmn00@gmail.com>
2025-01-21 17:51:01 +0530

    removed cors

--- server/app.js
+++ server/app.js
@@ -10,21 +10,23 @@ const PORT = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL,'http://localhost:3000', process.env.ORIGINAL],
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000",
+    process.env.ORIGINAL,
+  ],
   credentials: false,
   methods: ["GET", "POST", "PUT", "DELETE"], //access-control-allow-credentials:true
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(express.json());
-
-app.use(cors( ));
+app.use(cors());
 // app.use('/' , express.static(path.join(__dirname, '../client/dist')))
 app.use("/api/v1/categories", catRouter);
 app.use(errorHandler);
 // mongo db connection
 connectDB();
-app.listen(PORT, async (req,res) => {
-   
+app.listen(PORT, async (req, res) => {
   console.log(`Server running at  ${PORT} in ${process.env.NODE_ENV} mode`);
 });
